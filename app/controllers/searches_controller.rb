a496c78132e4e494d726f56d2b95501edc20abbd
LucasDC <46499843+LucasDCunha@users.noreply.github.com>
2025-07-31 10:28:15 -0300

    view and controller

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,23 @@
+class SearchesController < ApplicationController
+  def index
+    @popular_searches = SearchQuery.group(:text).order('count_id DESC').count(:id)
+  end
+
+  def create
+    ip = request.remote_ip
+    session = UserSession.find_or_create_by(ip: ip)
+
+    new_text = params[:text].to_s.strip
+    return redirect_to root_path if new_text.blank?
+
+    last_search = session.search_queries.last
+
+    if last_search && new_text.start_with?(last_search.text)
+      last_search.update(text: new_text)
+    else
+      session.search_queries.create(text: new_text)
+    end
+
+    redirect_to root_path
+  end
+end
