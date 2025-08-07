3e4f180a62b8aa033bf649eab0d34816334ada4f
LucasDC <46499843+LucasDCunha@users.noreply.github.com>
2025-08-07 14:33:45 -0300

    add reset btn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,21 +6,30 @@ class SearchesController < ApplicationController
   def create
     ip = request.remote_ip
     session = UserSession.find_or_create_by(ip: ip)
-
+  
     new_text = params[:text].to_s.strip
-    return redirect_to root_path if new_text.blank?
-
+    return head :ok if new_text.blank?
+  
     last_search = session.search_queries.last
-
+  
     if last_search &&
-      new_text.start_with?(last_search.text) &&
-      last_search.created_at > 30.seconds.ago
-
+       new_text.start_with?(last_search.text) &&
+       last_search.created_at > 30.seconds.ago
       last_search.update(text: new_text)
     else
       session.search_queries.create(text: new_text)
     end
+  
+    @popular_searches = SearchQuery.group(:text).order('count_id DESC').count(:id)
+  
+    respond_to do |format|
+      format.js   # create.js.erb
+      format.html { redirect_to root_path }
+    end
+  end
 
-    redirect_to root_path
+  def reset
+    SearchQuery.delete_all
+    @popular_searches = {}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     root 'searches#index'
-    resources :searches, only: [:index, :create]
+    resources :searches, only: [:index, :create] do
+        delete :reset, on: :collection
+    end
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
